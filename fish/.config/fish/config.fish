ab450a70515d6e425740c01aae847e273c8be49e
drzippit <drzippit@gmail.com>
2025-08-31 21:45:19 +0200

    add hyprlock

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,13 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # Enable !!
-function last_history_item; echo $history[1]; end 
+function last_history_item
+    echo $history[1]
+end
 abbr -a !! --position anywhere --function last_history_item
 
 zoxide init fish | source
 alias cd=z
 alias vim=nvim
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+alias bt=bluetui
+alias wf=impala
